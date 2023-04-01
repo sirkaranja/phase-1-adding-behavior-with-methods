07df0d02a51911bb4c06bb514904cf385d0736a2
Daniel Karanja <sirkaranja12@gmail.com>
2023-04-01 10:46:49 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Cat{
+    constructor(name){
+        this.name=name;
+    }
+    speak(){
+        return `${this.name} says Meows`;
+    }
+}
+
+class Dog{
+    constructor(name){
+        this.name=name;
+}
+speak(){
+    return `${this.name} says Woofs!`;
+}
+}
+class Bird{
+    constructor(name){
+        this.name=name;
+}
+speak(){
+    if (`${this.name}` = male){
+        return `It/s me ${this.name}, the Parrot! `;
+    }
+    else{
+        return `${this.name} says squawk!  `;
+    }
+}
+}
+    
